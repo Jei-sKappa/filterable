1ae61d2d91a169457e2cf426d92626b65fee4c33
Jei-sKappa <guzzojacopo95@gmail.com>
2023-09-11 00:37:38 +0200

    feat(examples): :tada: Created Example App

---
+++ example/lib/domain/models/person.dart
@@ -0,0 +1,26 @@
+import 'package:filterable/filterable.dart';
+import 'package:filterable_annotation/filterable_annotation.dart';
+
+part 'person.filterable.dart';
+
+@filterable
+class Person {
+  Person({
+    required this.name,
+    required this.surname,
+    required this.age,
+    required this.dateOfBirth,
+  });
+
+  final String name;
+
+  final String surname;
+
+  @ValueFilter()
+
+  final int age;
+
+  @ValueFilter()
+  @RangeFilter()
+  final DateTime dateOfBirth;
+}
