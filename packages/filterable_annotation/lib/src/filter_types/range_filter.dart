539f17ef99a839433d338f65a9a5e4f8185739bc
Jei-sKappa <guzzojacopo95@gmail.com>
2023-09-13 12:00:00 +0200

    refactor(filterable_annotation): :fire: Removed Default Values

--- packages/filterable_annotation/lib/src/filter_types/range_filter.dart
+++ packages/filterable_annotation/lib/src/filter_types/range_filter.dart
@@ -7,18 +7,5 @@ class RangeFilter implements FilterType {
   /// Creates a new [RangeFilter] instance.
   ///
   /// Only required when the default behavior is not desired.
-  const RangeFilter({
-    this.defaultMin,
-    this.defaultMax,
-    this.includeDefaultsInConstructor = true,
-  });
-
-  /// The default value for the minimum value of the range.
-  final String? defaultMin;
-
-  /// The default value for the maximum value of the range.
-  final String? defaultMax;
-
-  /// Whether the field should be included in the generated constructor.
-  final bool includeDefaultsInConstructor;
+  const RangeFilter();
 }
--- packages/filterable_annotation/lib/src/filter_types/value_filter.dart
+++ packages/filterable_annotation/lib/src/filter_types/value_filter.dart
@@ -7,14 +7,5 @@ class ValueFilter implements FilterType {
   /// Creates a new [ValueFilter] instance.
   ///
   /// Only required when the default behavior is not desired.
-  const ValueFilter({
-    this.defaultValue,
-    this.includeDefaultsInConstructor = true,
-  });
-
-  /// The default value for the field.
-  final String? defaultValue;
-
-  /// Whether the field should be included in the generated constructor.
-  final bool includeDefaultsInConstructor;
+  const ValueFilter();
 }
