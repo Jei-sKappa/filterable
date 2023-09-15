0ede941c8f1fda7f44fb7fe4f81ba0afd7bff6a2
Jei-sKappa <guzzojacopo95@gmail.com>
2023-09-15 02:02:08 +0200

    feat(filterable_annotation): :sparkles: Ignore

--- packages/filterable_annotation/lib/filterable_annotation.dart
+++ packages/filterable_annotation/lib/filterable_annotation.dart
@@ -4,3 +4,4 @@ library filterable_annotation;
 export 'src/field_key/field_key.dart';
 export 'src/filter_types/filter_type.dart';
 export 'src/filterable_annotation.dart';
+export 'src/ignore/ignore.dart';
